f443baa71cf647ec7bdc405f8e96bdb279a38685
tuannshe160097 <tuannshe160097@fpt.edu.vn>
2022-02-14 22:20:57 +0700

    Create PropertyTypeDao.java

---
+++ Source Code/Thuedi/src/java/DAO/PropertyTypeDao.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Model.PropertyType;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Admin
+ */
+public class PropertyTypeDao extends DBContext {
+    public Collection<PropertyType> select() {
+        String sql = "SELECT * FROM [thuedi].[dbo].[Sub_district]";
+        Collection<PropertyType> propertyType = new ArrayList<>();
+        
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            
+            ResultSet rs = st.executeQuery();
+            while(rs.next()) {
+                propertyType.add(new PropertyType(rs.getInt(1), rs.getString(2)));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(PostDao.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        return propertyType;
+    }
+}
