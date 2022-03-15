1e997ad087b259bb6ab41b7c274f3a0834cf59d5
tuannshe160097 <tuannshe160097@fpt.edu.vn>
2022-03-15 23:42:54 +0700

    Add Manage bookmark

--- Source Code/Thuedi/src/java/DAO/BookmarkDao.java
+++ Source Code/Thuedi/src/java/DAO/BookmarkDao.java
@@ -6,6 +6,8 @@
 package DAO;
 
 import Model.Bookmark;
+import Model.Post;
+import Model.PostBookmark;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -20,22 +22,60 @@ import java.util.logging.Logger;
  * @author Admin
  */
 public class BookmarkDao extends DBContext {
-    public Collection<Bookmark> select() {
-        String sql = "SELECT [Id]\n"
-                + "      ,[Post_id]\n"
-                + "      ,[User_id]\n"
-                + "  FROM [thuedi].[dbo].[Bookmark]";
-        Collection<Bookmark> bookmarks = new ArrayList<>();
-
+    public Collection<PostBookmark> selectBookmarkedPost(int id) {
+        String sql = "SELECT p.[Id] \n"
+                + "      ,p.[User_id]\n"
+                + "      ,p.[Create_date]\n"
+                + "      ,p.[Title]\n"
+                + "      ,p.[Detail]\n"
+                + "      ,p.[Price]\n"
+                + "      ,p.[Area]\n"
+                + "      ,p.[Number_of_bedrooms]\n"
+                + "      ,p.[Number_of_restrooms]\n"
+                + "      ,p.[Direction]\n"
+                + "      ,p.[Address]\n"
+                + "      ,p.[Address_detail]\n"
+                + "      ,p.[Property_type_id]\n"
+                + "      ,p.[Accept_covid_patient]\n"
+                + "	  ,b.[Id] as Bookmark_id\n"
+                + "  FROM [thuedi].[dbo].[Post] p\n"
+                + "  INNER JOIN [thuedi].[dbo].[Bookmark] b \n"
+                + "  ON p.[Id] = b.[Post_id]\n"
+                + "  WHERE b.[User_id] = ?";
+        
+        Collection<PostBookmark> posts = new ArrayList<>();
+        
         try {
             PreparedStatement st = connection.prepareStatement(sql);
-
+            st.setInt(1, id);
+            
             ResultSet rs = st.executeQuery();
-            while (rs.next()) {
-                bookmarks.add(new Bookmark(rs.getInt("Id"), rs.getInt("Post_id"), rs.getInt("User_ud")));
+            while(rs.next()) {
+                posts.add(new PostBookmark(
+                        new Post(
+                                rs.getInt("Id"),
+                                rs.getInt("User_id"),
+                                rs.getDate("Create_date"),
+                                rs.getString("Title"),
+                                rs.getString("Detail"),
+                                rs.getInt("Price"),
+                                rs.getFloat("Area"),
+                                rs.getInt("Number_of_bedrooms"),
+                                rs.getInt("Number_of_restrooms"),
+                                rs.getString("Direction"),
+                                rs.getInt("Address"),
+                                rs.getString("Address_detail"),
+                                rs.getInt("Property_type_id"),
+                                rs.getBoolean("Accept_covid_patient")),
+                        new Bookmark(
+                                rs.getInt("Bookmark_id"),
+                                rs.getInt("Id"),
+                                id)
+                ));
             }
         } catch (SQLException ex) {
             Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
         } finally {
             try {
                 connection.close();
@@ -43,9 +83,9 @@ public class BookmarkDao extends DBContext {
                 Logger.getLogger(PostDao.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-
-        return bookmarks;
-    }
+        
+        return posts;
+    } 
     
     public int insert(Bookmark bookmark) {
         String sql = "INSERT INTO [dbo].[Bookmark]\n"
@@ -77,5 +117,26 @@ public class BookmarkDao extends DBContext {
                 Logger.getLogger(PostDao.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+    }
+    
+    public int delete(int id) {
+        String sql = "DELETE FROM [dbo].[Bookmark]\n"
+                + "      WHERE Id = ?";
+
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, id);
+
+            return st.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(PostDao.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return 0;
     }
 }
